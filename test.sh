16da98f14f12d846b7b9a6fa2bffa5065cdf0a4f
zxcve <zxcve@vt.edu>
2017-03-14 10:54:55 -0400

    Error checking in shell script`

--- test.sh
+++ test.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "./test.sh <levels>"
+    echo "Max level allowed is 5"
+    exit -1
+fi
+
+if [ "$1" -lt "1" ]
+  then
+    echo "Minimum 1 level required"
+    echo "./test.sh <levels>"
+    echo "Max level allowed is 5"
+    exit -1
+fi
+
+if [ "$1" -gt "5" ]
+  then
+    echo "Maximum 5 levels allowed"
+    echo "./test.sh <levels>"
+    exit -1
+fi
+
 ./test $1 &
 pid=$!
 sleep 1
